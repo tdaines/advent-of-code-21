f07357e341ee071e77558a7459a2f4f284a82fc2
Tyler Daines <tyler.daines@edgecast.com>
2021-12-02 19:23:53 -0800

    Added tests for day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -19,13 +19,17 @@ fn calculate_num_increases(measurements: &[u32]) -> u32 {
 
     for depth in measurements {
         if *depth > current_depth {
-            num_increases = num_increases + 1;
+            num_increases += 1;
         }
 
         current_depth = *depth;
     }
 
-    num_increases - 1
+    if num_increases > 0 {
+        num_increases -= 1;
+    }
+
+    num_increases
 }
 
 fn calculate_window_increases(measurements: &[u32]) -> u32 {
@@ -35,11 +39,75 @@ fn calculate_window_increases(measurements: &[u32]) -> u32 {
     for depths in measurements.windows(3) {
         let sum: u32 = depths.iter().sum();
         if sum > current_window_sum {
-            num_increases = num_increases + 1;
+            num_increases += 1;
         }
 
         current_window_sum = sum;
     }
 
-    num_increases - 1
+    if num_increases > 0 {
+        num_increases -= 1;
+    }
+
+    num_increases
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    mod test_calculate_num_increases {
+        use super::*;
+
+        #[test]
+        fn empty_vec() {
+            let measurements = Vec::new();
+            assert_eq!(calculate_num_increases(&measurements), 0);
+        }
+
+        #[test]
+        fn vec_with_one_item() {
+            let measurements = Vec::new();
+            assert_eq!(calculate_num_increases(&measurements), 0);
+        }
+
+        #[test]
+        fn no_increases() {
+            let measurements = vec![5, 4, 3, 2, 1, 0];
+            assert_eq!(calculate_num_increases(&measurements), 0);
+        }
+
+        #[test]
+        fn some_increases() {
+            let measurements = vec![5, 4, 6, 7, 3, 8];
+            assert_eq!(calculate_num_increases(&measurements), 3);
+        }
+    }
+
+    mod test_calculate_window_increases {
+        use super::*;
+
+        #[test]
+        fn empty_vec() {
+            let measurements = Vec::new();
+            assert_eq!(calculate_window_increases(&measurements), 0);
+        }
+
+        #[test]
+        fn vec_with_one_item() {
+            let measurements = Vec::new();
+            assert_eq!(calculate_window_increases(&measurements), 0);
+        }
+
+        #[test]
+        fn no_increases() {
+            let measurements = vec![5, 4, 3, 2, 1, 0];
+            assert_eq!(calculate_window_increases(&measurements), 0);
+        }
+
+        #[test]
+        fn some_increases() {
+            let measurements = vec![5, 4, 6, 7, 3, 8];
+            assert_eq!(calculate_window_increases(&measurements), 2);
+        }
+    }
 }
